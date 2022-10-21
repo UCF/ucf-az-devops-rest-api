f079a5b10a34c94964342aaf33cc1b47bc7073db
Bradley Smith <bradley_e_smith@yahoo.com>
2022-10-21 03:03:11 +0100

    try with enque

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -124,8 +124,8 @@ function wp_devops_wiql($atts = [], $content = null) {
 //	print '<script type="text/javascript" charset="utf8" src="' . plugins_url( '/js/init.js', __FILE__ ) . '"></script>';
 	
 print '	
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
 	<script>
 
 (function() {
--- readme.txt
+++ readme.txt
@@ -7,8 +7,8 @@ Author: Bradley Smith
 Requires PHP: 7.0
 Requires at least: 5.6
 Tested up to: 5.7
-Stable tag: 2.00
-Version: 2.00
+Stable tag: 2.01
+Version: 2.01
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -26,5 +26,8 @@ Welcome to Brad's Azure Devops REST API 4 UCF
 
 
 == Changelog ==
+= v2.01 [10/18/2022]
+* small change for testing
+
 = v2.00 [10/18/2022]
 * Initial released
