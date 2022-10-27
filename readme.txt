0c80353bfb96b7e74821064e50edbeaea369cc83
Bradley Smith <bradley_e_smith@yahoo.com>
2022-10-27 23:09:20 +0100

    auto git push with script

--- readme.txt
+++ readme.txt
@@ -7,8 +7,8 @@ Author: Bradley Smith
 Requires PHP: 7.0
 Requires at least: 5.6
 Tested up to: 5.7
-Stable tag: 2.57
-Version: 2.57
+Stable tag: 2.58
+Version: 2.58
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- ucf-az-devops-rest-api.php
+++ ucf-az-devops-rest-api.php
@@ -3,7 +3,7 @@
 * Plugin Name: Brad's Azure Devops REST API 4 UCF
 * Plugin URI: https://www.yourwebsiteurl.com/
 * Description: Brad's Azure Devops REST API 4 UCF
-* Version: 2.57
+* Version: 2.58
 * Author: Bradley Smith
 * Author URI: http://yourwebsiteurl.com/
 **/
