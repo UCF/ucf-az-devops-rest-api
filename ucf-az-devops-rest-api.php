3d41f8e3103381be80b2156479f483d86c4c3ac7
Bradley Smith <bradley_e_smith@yahoo.com>
2022-11-05 03:32:02 +0100

    auto git push with script

--- ucf-az-devops-rest-api.php
+++ ucf-az-devops-rest-api.php
@@ -3,7 +3,7 @@
 * Plugin Name: Brad's Azure Devops REST API 4 UCF
 * Plugin URI: https://www.yourwebsiteurl.com/
 * Description: Brad's Azure Devops REST API 4 UCF
-* Version: 2.75
+* Version: 2.76
 * Author: Bradley Smith
 * Author URI: http://yourwebsiteurl.com/
 **/
