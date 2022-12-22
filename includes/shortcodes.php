642e9f6b51d543dda6f78819cc79471e5b79f5c2
Bradley Smith <bradley_e_smith@yahoo.com>
2022-12-22 02:03:29 +0000

    auto git push with script

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1211,6 +1211,7 @@ function wp_devops_query($atts = [], $content = null) {
 		$ucf_workitem_url = $ucf_workItems[$i]->{'url'};
 		
 		$item_url =  "https://dev.azure.com/" . $Organization . "/" . $Project . "/_apis/wit/workitems?ids=" . $ucf_workitem_id . '&$expand=all&api-version=6.0';
+		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $item_url);
 		curl_setopt($curl, CURLOPT_POST, FALSE);
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
